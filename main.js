8a315f685f4bae63852f0a29e5e48a3bb0137996
Sanvi-Agarwal <85011956+Sanvi-Agarwal@users.noreply.github.com>
2021-07-31 08:12:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+   var updatedImages = ["great grandfather.jpg", "Grandparents.jpg" , "4 people.jpg", "whole family.jpg"];
+            var names = ["Family Book","Great GrandFather", "GrandParents", "Father, Mother, Brother, Me", "Family"];
+var i = 0;
+function update()
+{
+    i++;
+    var numbers_of_family_photo_in_array = 5
+    if(i > numbers_of_family_photo_in_array)
+      {
+          i = 0;
+      } 
